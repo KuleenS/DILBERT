f139a3f950ef479139e7c5dde2026773039f2013
Kuleen Sasse <cometku@gmail.com>
2022-03-25 02:56:15 +0000

    Small Ease of use tweaks

---
+++ models/__init__.py
@@ -0,0 +1,2 @@
+from .bert_ranker import RankingMapper
+from .random_sampler import RandomSampler
--- runUMLS.sh
+++ runUMLS.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 DATA_DIR=./data/LITCOINDILBERTPREPARED/
 TRIPLETS_FOLDER=./data/corpora/litcoin/
 TRIPLETS_FILE=train_neg30_pos30_parents2_random.txt
 BERT_MODEL=./data/pretrained_models/biobert-base-cased-v1.2
 OUTPUT_DIR=./tmp/litcoin
-PYTHON_PATH=/home/ksasse/code/alzhiemers/DILBERT
+
 
 
 PREPARE_TRIPLETS=true
@@ -12,8 +13,8 @@ EVAL_MODEL=true
 
 
 if $PREPARE_TRIPLETS; then
-  mkdir $TRIPLETS_FOLDER
-  PYTHONPATH=/home/ksasse/code/alzhiemers/DILBERT python data_utils/convert_to_triplet_dataset.py --input_data $DATA_DIR/train \
+  mkdir -p $TRIPLETS_FOLDER
+  PYTHONPATH=$(pwd) python data_utils/convert_to_triplet_dataset.py --input_data $DATA_DIR/train \
                                        --vocab $DATA_DIR/MSHOMIMVocab.txt \
                                        --hierarchy_aware \
                                        --hierarchy $DATA_DIR/UMLSgraph.csv \
@@ -22,14 +23,15 @@ if $PREPARE_TRIPLETS; then
 fi
 
 if $TRAIN_MODEL; then
-  PYTHONPATH=/home/ksasse/code/alzhiemers/DILBERT python train_sentence_bert.py --path_to_bert_model $BERT_MODEL \
+  mkdir -p $OUTPUT_DIR
+  PYTHONPATH=$(pwd) python train_sentence_bert.py --path_to_bert_model $BERT_MODEL \
                                 --data_folder $TRIPLETS_FOLDER \
                                 --triplets_file $TRIPLETS_FILE \
                                 --output_dir $OUTPUT_DIR
 fi
 
 if $EVAL_MODEL; then
-  PYTHONPATH=/home/ksasse/code/alzhiemers/DILBERT python eval_bert_ranking.py --model_dir $OUTPUT_DIR \
+  PYTHONPATH=$(pwd) python eval_bert_ranking.py --model_dir $OUTPUT_DIR \
                             --data_folder $DATA_DIR/test \
                             --vocab $DATA_DIR/MSHOMIMVocab.txt  > $DATA_DIR/eval_results.txt
 fi
