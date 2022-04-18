99fb2141758501af005a6a7040487300f676df45
Kuleen Sasse <56900136+KuleenS@users.noreply.github.com>
2022-04-18 00:07:47 -0400

    Update UMLS_hierarchy.py
    Adding comments

--- hierarchy/UMLS_hierarchy.py
+++ hierarchy/UMLS_hierarchy.py
@@ -3,13 +3,17 @@ from typing import Dict
 from tqdm import tqdm
 import ast
 
+
+#UMLS version of MESH Graph
 class UMLSGraph:
     def __init__(self, umls_path: str) -> None:
+        #reads CSV and makes tree
         self.umls = pd.read_csv(umls_path)
         self.tree = {}
         self.create_tree(self.umls)
 
     def create_tree(self, mesh: pd.DataFrame) -> Dict[str, Dict[str, str]]:
+        #loops through CSV and gets the fills in the child and parents of each CUI
         for row_idx, UMLS_ENTRY in tqdm(enumerate(mesh.itertuples(index=False)), total=mesh.shape[0]):
             node = self.get_node(UMLS_ENTRY)
             if UMLS_ENTRY.REL=='CHD':
@@ -18,6 +22,7 @@ class UMLSGraph:
                 node['children'].extend(ast.literal_eval(UMLS_ENTRY.CODE_Second))
 
     def get_node(self, row):
+        #returns the node of the tree
         if row.CODE_First in self.tree:
             node = self.tree[row.CODE_First]
         else:
